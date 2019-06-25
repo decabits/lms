535316a8e36c5c0b8e0051b00d636dec9309b2ea
Decabits Learning Management System <sikand.jasmeet3@gmail.com>
2019-06-25 16:07:31 +0530

    updated model and schema for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+  namespace :v1 do
+    # resources :contacts
+    resources :sessions, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'course#home'
   resources :tracks
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+
 end
