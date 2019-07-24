e123448f22cd0957b083272a458dcb763b7ac8d4
Decabits Learning Management System <sikand.jasmeet3@gmail.com>
2019-07-24 14:27:02 +0530

    CRUD features added

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -5,5 +5,44 @@ class TracksController < ApplicationController
 	def show
       @track = Track.find(params[:id])
       @items = @track.track_items
+  end
+  def destroy
+    @track = Track.find(params[:id])
+    @track.destroy
+    flash[:notice] = "Track was successfully deleted" 
+    redirect_to 'tracks_path'
+  end
+
+  def new
+    @track = Track.new
+  end
+
+  def edit
+    @track = Track.find(params[:id])
+  end
+
+  def create 
+    @track = Track.new(track_params)
+    if @track.save
+      flash[:notice] = "Track was successfully created"
+      redirect_to tracks_path(@track)
+    else
+      render "new"
     end
+  end
+
+  def update
+    @track = Track.find(params[:id])
+    if @track.update(track_params)
+      flash[:notice]
+      redirect_to tracks_path(@track)
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def track_params
+    params.require(:track).permit(:name, :description)
+  end
 end
